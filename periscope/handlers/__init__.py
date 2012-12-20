d741081cf0589d0e5e4b20510130adb9f08ba5b7
Ahmed El-Hassany <ahassany@indiana.edu>
2012-12-20 15:50:06 -0500

    Restructure handlers.py to included in multiple files

---
+++ periscope/handlers/__init__.py
@@ -0,0 +1,462 @@
+#!/usr/bin/env python
+"""
+Periscope HTTP(s) Handlers.
+"""
+
+
+MIME = {
+    'HTML': 'text/html',
+    'JSON': 'application/json',
+    'PLAIN': 'text/plain',
+    'SSE': 'text/event-stream',
+    'PSJSON': 'application/perfsonar+json',
+    'PSXML': 'application/perfsonar+xml',
+}
+
+SCHEMAS = {
+    'networkresource': 'http://unis.incntre.iu.edu/schema/20120709/networkresource#',
+    'node': 'http://unis.incntre.iu.edu/schema/20120709/node#',
+    'domain': 'http://unis.incntre.iu.edu/schema/20120709/domain#',
+    'port': 'http://unis.incntre.iu.edu/schema/20120709/port#',
+    'link': 'http://unis.incntre.iu.edu/schema/20120709/link#',
+    'path': 'http://unis.incntre.iu.edu/schema/20120709/path#',
+    'network': 'http://unis.incntre.iu.edu/schema/20120709/network#',
+    'topology': 'http://unis.incntre.iu.edu/schema/20120709/topology#',
+    'blipp': 'http://unis.incntre.iu.edu/schema/20120709/blipp#',
+    'metadata': 'http://unis.incntre.iu.edu/schema/20120709/metadata#',
+    'service' : "http://unis.incntre.iu.edu/schema/20120709/service#"
+}
+
+
+# TODO (AH): cache common schemas locally
+# TODO (AH): This is a very ugly way of handling cache!
+CACHE = {
+    "http://json-schema.org/draft-03/links#": {
+        "additionalProperties": True,
+        "type": "object"
+    },
+    "http://json-schema.org/draft-03/hyper-schema#": {
+        "additionalProperties": True,
+        "type": "object"
+    },
+}
+
+
+
+import copy
+import json
+import re
+import functools
+import jsonpointer
+from jsonpath import jsonpath
+from netlogger import nllog
+import time
+import urllib2
+import traceback
+from tornado.ioloop import IOLoop
+import tornado.gen as gen
+import tornado.web
+from tornado.httpclient import HTTPError
+from tornado.httpclient import AsyncHTTPClient
+import pymongo
+if pymongo.version_tuple[1] > 1:
+    from bson.objectid import ObjectId
+else:
+    from pymongo.objectid import ObjectId
+
+from urllib import urlencode
+
+from periscope.db import DBLayer
+from periscope.db import dumps_mongo
+from periscope.models import ObjectDict
+from periscope.models import NetworkResource
+from periscope.models import HyperLink
+from periscope.models import Topology
+from periscope.models import schemaLoader
+from periscope.models import JSONSchemaModel
+import periscope.utils as utils
+from asyncmongo.errors import IntegrityError, TooManyConnections
+
+
+from periscope.handlers.sse_handler import SSEHandler
+from periscope.handlers.networkresource_handler import NetworkResourceHandler
+from periscope.handlers.collection_handler import CollectionHandler
+
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def initialize(self, base_url, resources):
+        self._resources = resources
+    
+    def get(self):
+        links = []
+        for resource in self._resources:
+            href = "%s://%s%s" % (self.request.protocol,
+                self.request.host, self.reverse_url(resource))
+            links.append({"href": href, "rel": "full"})
+        self.set_header("Content-Type", MIME["JSON"])
+        self.write(json.dumps(links, indent=4))
+
+              
+class EventsHandler(NetworkResourceHandler):        
+        
+    @tornado.web.asynchronous
+    @tornado.web.removeslash
+    def post(self, res_id=None):
+        # Check if the schema for conetnt type is known to the server
+        if self.accept_content_type not in self.schemas_single:
+            message = "Schema is not defiend fot content of type '%s'" % \
+                        (self.accept_content_type)
+            self.send_error(500, message=message)
+            return
+        # POST requests don't work on specific IDs
+        if res_id:
+            message = "NetworkResource ID should not be defined."
+            self.send_error(400, message=message)
+            return
+
+        # Load the appropriate content type specific POST handler
+        if self.content_type == MIME['PSJSON']:
+            self.post_psjson()
+        else:
+            self.send_error(500,
+                message="No POST method is implemented fot this content type")
+            return
+        return
+
+    def on_post(self, request, error=None, res_refs=None, return_resources=True, last=True):
+        """
+        HTTP POST callback to send the results to the client.
+        """
+        
+        if error:
+            if isinstance(error, IntegrityError):
+                self.send_error(409,
+                    message="Could't process the POST request '%s'" % \
+                        str(error).replace("\"", "\\\""))
+            else:
+                self.send_error(500,
+                    message="Could't process the POST request '%s'" % \
+                        str(error).replace("\"", "\\\""))
+            return                
+        self.set_status(201)
+        #pool = self.application.async_db._pool
+        #pool.close()
+        self.finish()
+
+    def verify_metadata(self,response, collection_size,post_body):
+        if response.error:
+            self.send_error(400, message="metadata is not found '%s'." % response.error)
+        else:
+            body=json.loads(response.body)
+            if body["id"] not in self.application.sync_db.collection_names():
+                self.application.get_db_layer(body["id"],"ts","ts",True,collection_size)
+                self.set_header("Location",
+                    "%s/data/%s" % (self.request.full_url(), body["id"]))
+                callback = functools.partial(self.on_post,
+                                             res_refs=None, return_resources=True)
+                post_body["ts"] = int(time.time() * 1000000)
+                post_body["id"] = body["id"]
+                self.dblayer.insert(post_body, callback=callback)
+            else:
+                self.send_error(401, message="event collection exists already")  
+            
+    def post_psjson(self):
+        """
+        Handles HTTP POST request with Content Type of PSJSON.
+        """
+        profile = self._validate_psjson_profile()
+        if not profile:
+            return
+        try:
+            body = json.loads(self.request.body)
+        except Exception as exp:
+            self.send_error(400, message="malformatted json request '%s'." % exp)
+            return
+        
+
+        callback = functools.partial(self.verify_metadata,
+                                     collection_size=body["collection_size"], post_body=body)
+        
+        http_client = AsyncHTTPClient()
+        http_client.fetch(body["metadata_URL"], callback)        
+
+    def del_stat_fields(self,generic):
+        generic.pop("ns",None)
+        generic.pop("numExtents",None)
+        generic.pop("nindexes",None)
+        generic.pop("lastExtentSize",None)
+        generic.pop("paddingFactor",None)
+        generic.pop("flags",None)
+        generic.pop("totalIndexSize",None)
+        generic.pop("indexSizes",None)
+        generic.pop("max",None)
+        generic.pop("ok",None)
+        if generic["capped"] == 1:
+            generic["capped"]="Yes"
+        else:
+            generic["capped"]="No"
+
+       
+    def generate_response(self,query,mid,response,index):
+        try:
+            command={"collStats":mid,"scale":1}
+            generic = self.application.sync_db.command(command)
+        except Exception as exp:
+            self.send_error(400, message="At least one of the metadata ID is invalid.")
+            return
+              
+        self.del_stat_fields(generic)
+        specific={}
+        if 'ts' in self.request.arguments.keys():       
+            criteria=self.request.arguments['ts'][0].split('=')
+            
+            if criteria[0]=='gte':
+                specific["startTime"]=int(criteria[1])
+            if criteria[0]=='lte':
+                specific["endTime"]=int(criteria[1])
+            
+            if self.request.arguments['ts'].__len__() > 1 :            
+                criteria=self.request.arguments['ts'][1].split('=')
+                if criteria[0]=='gte':
+                    specific["startTime"]=int(criteria[1])
+                if criteria[0]=='lte':
+                    specific["endTime"]=int(criteria[1])
+            
+            db_query=copy.deepcopy(query)
+            del db_query["$and"][index]
+            specific["numRecords"]=self.application.sync_db[mid].find(db_query).count()
+            
+        response.insert(0,{})
+        response[0]["mid"]=mid
+        response[0]["generic"]=generic
+        response[0]["queried"]=specific
+
+                                            
+    @tornado.web.asynchronous
+    @tornado.web.removeslash
+    def get(self, res_id=None):
+        """Handles HTTP GET"""
+        accept = self.accept_content_type
+        if res_id:
+            self._res_id = unicode(res_id)
+        else:
+            self._res_id = None
+            
+        parsed = self._parse_get_arguments()
+        query = parsed["query"]
+        fields = parsed["fields"]
+        limit = parsed["limit"]
+        is_list = not res_id
+        self.set_header("Content-Type", "application/json")
+        if query.__len__() == 0:
+            if self._res_id is None:
+                q = {}
+            else:
+                q = {"id": self._res_id}
+            cursor =  self.application.sync_db["events_cache"].find(q)
+            index = -1
+            response = []
+            obj = next(cursor,None)
+            while obj:
+                index = index+1
+                mid = obj["metadata_URL"].split('/')[obj["metadata_URL"].split('/').__len__() - 1]
+                self.generate_response(query,mid,response,index)
+                obj = next(cursor, None)
+            try:
+                json_response = dumps_mongo(response, indent=2)
+                self.write(json_response)
+                self.finish()
+            except Exception as exp:
+                self.send_error(400, message="1 At least one of the metadata ID is invalid.")
+                return                
+        else:
+            index=-1
+            response=[]
+            for d in query["$and"]:
+                index=index+1
+                if 'mids' in d.keys():
+                    if isinstance(d["mids"],dict):
+                        for m in d['mids']['$in']:
+                            self.generate_response(query,m,response,index)
+                    else:
+                        self.generate_response(query,d['mids'],response,index)
+            try:
+                json_response = dumps_mongo(response, indent=2)
+                self.write(json_response)
+                self.finish()
+            except Exception as exp:
+                self.send_error(400, message="1 At least one of the metadata ID is invalid.")
+                return                
+        
+class DataHandler(NetworkResourceHandler):        
+        
+    @tornado.web.asynchronous
+    @tornado.web.removeslash
+    def post(self, res_id=None):
+        # Check if the schema for conetnt type is known to the server
+        if self.accept_content_type not in self.schemas_single:
+            message = "Schema is not defiend fot content of type '%s'" % \
+                        (self.accept_content_type)
+            self.send_error(500, message=message)
+            return
+        # POST requests don't work on specific IDs
+        #if res_id:
+        #    message = "NetworkResource ID should not be defined."
+        #    self.send_error(400, message=message)
+        #    return
+        self._res_id=res_id
+        #Load the appropriate content type specific POST handler
+        if self.content_type == MIME['PSJSON']:
+            self.post_psjson()
+        else:
+            self.send_error(500,
+                message="No POST method is implemented for this content type")
+            return
+        return
+    
+    def on_post(self, request, error=None, res_refs=None, return_resources=True, last=True):
+        """
+        HTTP POST callback to send the results to the client.
+        """
+        
+        if error:
+            if isinstance(error, IntegrityError):
+                self.send_error(409,
+                    message="Could't process the POST request '%s'" % \
+                        str(error).replace("\"", "\\\""))
+            else:
+                self.send_error(500,
+                    message="Could't process the POST request '%s'" % \
+                        str(error).replace("\"", "\\\""))
+            return
+        
+        if return_resources:
+            query = {"$or": []}
+            for res_ref in res_refs:
+                query["$or"].append(res_ref)
+            self.dblayer.find(query, self._return_resources)
+        else:
+            if last:
+                accept = self.accept_content_type
+                self.set_header("Content-Type", accept + \
+                                " ;profile="+ self.schemas_single[accept])
+                if len(res_refs) == 1:
+                    self.set_header("Location",
+                                    "%s/%s" % (self.request.full_url(), res_refs[0][self.Id]))
+                
+                self.set_status(201)
+                #pool = self.application.async_db._pool
+                #pool.close()
+                self.finish()   
+                     
+    def post_psjson(self):
+        """
+        Handles HTTP POST request with Content Type of PSJSON.
+        """                    
+        profile = self._validate_psjson_profile()
+        if not profile:
+            return
+        try:
+            body = json.loads(self.request.body)
+        except Exception as exp:
+            self.send_error(400, message="malformatted json request '%s'." % exp)
+            return
+        if self._res_id:
+            res_refs =[]
+            if self._res_id in self.application.sync_db.collection_names():
+                callback = functools.partial(self.on_post,
+                        res_refs=res_refs, return_resources=False,last=True)
+                try:
+                    self.application.async_db[self._res_id].insert(body["data"], callback=callback)
+                except TooManyConnections:
+                    self.send_error(503, message="Too many DB connections")
+                    return
+            else:
+                self.send_error(400, message="The collection for metadata ID '%s' does not exist" % self._res_id)
+                return
+        else:
+            col_names = self.application.sync_db.collection_names()
+            data={}
+            for i in range(0,body.__len__()):
+                mid = body[i]['mid']
+                dataraw = body[i]['data']
+                if(mid in data.keys()):
+                    data[mid].extend(dataraw)
+                else :
+                    data[mid]=dataraw
+                   
+            mids = data.keys()
+            
+            for i in range(0,mids.__len__()):    
+                res_refs =[]
+                if mids[i] in col_names:
+                    if i+1 == mids.__len__():
+                        callback = functools.partial(self.on_post,
+                                                     res_refs=res_refs, return_resources=False,last=True)
+                    else:
+                        callback = functools.partial(self.on_post,
+                                                     res_refs=res_refs, return_resources=False,last=False)
+                    try:
+                        self.application.async_db[mids[i]].insert(data[mids[i]], callback=callback)
+                    except TooManyConnections:
+                        self.send_error(503, message="Too many DB connections")
+                        return
+                else:
+                    self.send_error(400, message="The collection for metadata ID '%s' does not exist" % mids[i])
+                    return
+
+    @tornado.web.asynchronous
+    @tornado.web.removeslash
+    def get(self, res_id=None):
+        """Handles HTTP GET"""
+        accept = self.accept_content_type
+        if res_id:
+            self._res_id = unicode(res_id)
+        else:
+            self.send_error(500, message="You need to specify the metadata ID in the URL while querying the data")
+            return
+        
+        parsed = self._parse_get_arguments()
+        query = parsed["query"]
+        fields = parsed["fields"]
+        fields["_id"] = 0
+        limit = parsed["limit"]
+        if limit == None:
+            limit = 1000000000
+
+        is_list = True #, not res_id
+        if query:
+            is_list = True
+        callback = functools.partial(self._get_on_response,
+                            new=True, is_list=is_list, query=query)
+        self._find(query, callback, fields=fields, limit=limit)
+
+    def _find(self, query, callback, fields=None, limit=None):
+        """Query the database.
+
+        Parameters:
+
+        callback: a function to be called back in case of new data.
+                callback function should have `response`, `error`,
+                and `new` fields. `new` is going to be True.
+        """
+        keep_alive = self.supports_streaming or self.supports_sse()
+        if self._res_id:
+            query[self.Id] = self._res_id
+        options = dict(query=query, callback=callback)#, await_data=True)
+        # Makes it a tailable cursor
+        if keep_alive and self._tailable:
+            options.update(dict(tailable=True, timeout=False))
+        if fields:
+            options["fields"] = fields
+        if limit:
+            options["limit"] = limit
+        if "sort" not in options:
+            options["sort"] = []
+        options["sort"].append(("ts", -1))
+        self._query = query
+        db_layer = self.application.get_db_layer(self._res_id, "ts", "ts",
+                        True,  5000)
+        query.pop("id", None)
+        self._cursor = db_layer.find(**options)        
---
+++ periscope/handlers/collection_handler.py
@@ -0,0 +1,246 @@
+#!/usr/bin/env python
+"""
+Periscope HTTP(s) Handlers.
+"""
+
+import copy
+import json
+import re
+import functools
+import jsonpointer
+from jsonpath import jsonpath
+from netlogger import nllog
+import time
+import urllib2
+import traceback
+from tornado.ioloop import IOLoop
+import tornado.gen as gen
+import tornado.web
+from tornado.httpclient import HTTPError
+from tornado.httpclient import AsyncHTTPClient
+import pymongo
+if pymongo.version_tuple[1] > 1:
+    from bson.objectid import ObjectId
+else:
+    from pymongo.objectid import ObjectId
+
+from urllib import urlencode
+
+from periscope.handlers.sse_handler import SSEHandler
+from periscope.db import DBLayer
+from periscope.db import dumps_mongo
+from periscope.models import ObjectDict
+from periscope.models import NetworkResource
+from periscope.models import HyperLink
+from periscope.models import Topology
+from periscope.models import schemaLoader
+from periscope.models import JSONSchemaModel
+import periscope.utils as utils
+from asyncmongo.errors import IntegrityError, TooManyConnections
+
+from periscope.handlers.networkresource_handler import NetworkResourceHandler
+from periscope.handlers import MIME, SCHEMAS
+
+
+class CollectionHandler(NetworkResourceHandler):
+    def initialize(self, collections, *args, **kwargs):
+        self._collections = collections
+        super(CollectionHandler, self).initialize(*args, **kwargs)
+        self._models_index = {}
+        self._dblayers_index = {}
+        self._cache = {}
+        for key, value in self._collections.items():
+            self._models_index[value["model_class"]] = key
+            dblayer = self.application.get_db_layer(value["collection_name"],
+                    value["id_field_name"],
+                    value["timestamp_field_name"],
+                    value["is_capped_collection"],
+                    value["capped_collection_size"]
+                )
+            self._dblayers_index[key] = dblayer
+    
+    @tornado.web.asynchronous
+    @tornado.web.removeslash
+    def post(self):
+        # Check if the schema for conetnt type is known to the server
+        if self.accept_content_type not in self.schemas_single:
+            message = "Schema is not defiend fot content of type '%s'" % \
+                        (self.accept_content_type)
+            self.send_error(500, message=message)
+            return
+        
+        # Load the appropriate content type specific POST handler
+        if self.content_type == MIME['PSJSON']:
+            self.post_psjson()
+        else:
+            self.send_error(500,
+                message="No POST method is implemented fot this content type")
+            return
+        return
+    
+    @gen.engine
+    def post_psjson(self):
+        """
+        Handles HTTP POST request with Content Type of PSJSON.
+        """
+        profile = self._validate_psjson_profile()
+        if not profile:
+            return
+        try:
+            body = json.loads(self.request.body)
+        except Exception as exp:
+            self.send_error(400, message="malformatted json request '%s'." % exp)
+            return
+        
+        try:
+            collections = []
+            if isinstance(body, list):
+                for item in body:
+                    collections.append(self._model_class(item,
+                        schemas_loader=schemaLoader))
+            else:
+                collections = [self._model_class(body,
+                        schemas_loader=schemaLoader)] 
+        except Exception as exp:
+            self.send_error(400, message="malformatted request " + str(exp))
+            return
+        
+        for collection in collections:
+            print "Validating", type(collection)
+            collection._validate()
+                
+        # Validate schema
+        try:
+            for collection in collections:
+                if collection.get("$schema", None) != \
+                    self.schemas_single[self.accept_content_type]:
+                     self.send_error(400,
+                     message="Not valid body '%s'; expecting $schema: '%s'." % \
+                     (collection.get("$schema", None), self.schemas_single[self.accept_content_type]))
+                     return
+                collection._validate()
+        except Exception as exp:
+            self.send_error(400, message="Not valid $schema '%s'." % exp)
+            return
+        
+        self._cache = {}
+        coll_reps = []
+        for collection in collections:
+            collection["selfRef"] = "%s/%s" % (self.request.full_url(), collection[self.Id])
+                    
+            # Convert JSONPath and JSONPointer Links to Hyper Links
+            ret = self._complete_href_links(collection, collection)
+            # Check if something went wrong
+            if ret < 0:
+                return
+            
+            res_refs = [
+                {
+                    self.Id: collection[self.Id],
+                    self.timestamp: collection[self.timestamp]
+                }
+            ]
+            has_error = False
+            keys_to_insert = []
+            http_client = AsyncHTTPClient()
+            
+            # Async calls to insert all the resources included in the request
+            responses = yield [
+                gen.Task(
+                    http_client.fetch,
+                    "%s://%s%s" % (self.request.protocol, self.request.host, self.reverse_url(key)),
+                    method = "POST",
+                    body = dumps_mongo(collection[key]),
+                    request_timeout=180,
+                    headers = {
+                        "Cache-Control": "no-cache",
+                        "Content-Type": MIME['PSJSON'],
+                        "Connection": "close"
+                        }
+                    )
+                    for key in self._collections.keys()
+                    if key in collection
+            ]
+            
+            for response in responses:
+                if response.code >= 400:
+                    self.send_error(response.code, message=response.body)
+                    return
+           
+            for key in self._collections:
+                if key not in collection:
+                    continue
+                for index in range(len(collection[key])):
+                    if 'selfRef' in collection[key][index]:
+                        collection[key][index] = {"href": collection[key][index]["selfRef"], "rel": "full"}
+            coll_reps.append(dict(collection._to_mongoiter()))
+        
+        callback = functools.partial(self.on_post, res_refs=res_refs)
+        self.dblayer.insert(coll_reps, callback=callback)
+            
+
+    def set_self_ref(self, resource):
+        """Assignes a selfRef to a resource"""
+        fullname = utils.class_fullname(resource)
+        if fullname not in self._models_index:
+            self.send_error(400,
+                message="Unrecognized resource type: %s" % type(resource))
+            return -1
+        resource_name = self._models_index[fullname]
+        resource_url = self.reverse_url(
+            self._collections[resource_name]["name"], resource[self.Id]) 
+        resource["selfRef"] = "%s://%s%s" % (
+            self.request.protocol, self.request.host, resource_url)
+        return 0
+            
+    def _complete_href_links(self, parent_collection, current):
+        """Resolves self hyperlinks (JSONPath and JSONPointers."""
+        if isinstance(current, HyperLink) or \
+            (isinstance(current, dict) and "href" in current):
+            if isinstance(current["href"], (unicode, str)):
+                resource = None
+                if current["href"] in self._cache:
+                    resource = self._cache[current["href"]]
+                elif current["href"].startswith("#"):
+                    resource = jsonpointer.resolve_pointer(parent_collection,
+                              current["href"][1:])
+                    if not resource:
+                        resource = "Unresolved"
+                elif current["href"].startswith("$"):
+                    path = jsonpath(parent_collection,
+                        current["href"], result_type="PATH")
+                    if path:
+                        resource = eval("parent_collection%s" % path[0].lstrip("$"))
+                    else:
+                        resource = "Unresolved"
+                self._cache[current["href"]] = resource
+                if resource and resource != "Unresolved":
+                    if "selfRef" not in resource:
+                        ret = self.set_self_ref(resource)
+                        if ret < 0:
+                            return ret
+                    current["href"] = resource["selfRef"]
+            return 0
+        elif isinstance(current, list):
+            keys = range(len(current))
+        elif isinstance(current, dict):
+            keys = current.keys()
+        else:
+           return 0
+        
+        for key in keys:
+            value = current[key]
+            if isinstance(value, (NetworkResource, Topology)) and \
+                "selfRef" not in value:
+                ret = self.set_self_ref(value)
+                if ret < 0:
+                    return ret
+            if isinstance(value, list) or isinstance(value, dict):
+                ret = self._complete_href_links(parent_collection, value)
+                if ret < 0:
+                    return ret
+        return 0
+
+
+
+ 
