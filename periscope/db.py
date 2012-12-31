ee64db6bd9e01035598f0afdeef3143cb7ab5734
Ahmed El-Hassany <ahassany@indiana.edu>
2012-12-30 20:38:25 -0500

    Step tworads refactoring DBLayer and adding support for motor

--- periscope/db.py
+++ periscope/db.py
@@ -2,33 +2,191 @@
 """
 Databases related classes
 """
-import time
+import abc
+import asyncmongo
 import functools
-from json import JSONEncoder
+import json
+import time
+from bson.objectid import ObjectId
+from bson.json_util import dumps
 from netlogger import nllog
 
+# Motor is optional
+MOTORINSTALLED = True
+try:
+    import motor
+except ImportError:
+    MOTORINSTALLED = False
 
-class MongoEncoder(JSONEncoder):
+
+DUMPS_MONGO = dumps
+OBJECTID = ObjectId
+
+
+class MongoEncoder(json.JSONEncoder):
     """Special JSON encoder that converts Mongo ObjectIDs to string"""
     def _iterencode(self, obj, markers=None):
         if isinstance(obj, ObjectId):
             return """ObjectId("%s")""" % str(obj)
         else:
-            return JSONEncoder._iterencode(self, obj, markers)
+            return json.JSONEncoder._iterencode(self, obj, markers)
 
-import pymongo
-if pymongo.version_tuple[1] > 1:
-    from bson.objectid import ObjectId
-    import bson.json_util
-    dumps_mongo = bson.json_util.dumps
-else:
-    from pymongo.objectid import ObjectId
-    import json
-    dumps_mongo = functools.partial(json.dumps, cls=MongoEncoder)
 
-import motor
+class AbstractDBCursor(object):
+    __metaclass__ = abc.ABCMeta
 
-class DBLayer(object, nllog.DoesLogging):
+    @abc.abstractproperty
+    def fetch_next(self):
+        pass
+
+    @abc.abstractmethod
+    def next_object(self):
+        pass
+
+    @abc.abstractmethod
+    def each(self, callback):
+        pass
+
+    @abc.abstractmethod
+    def to_list(self, length=None, callback=None):
+        pass
+
+    @abc.abstractmethod
+    def tail(self, callback):
+        pass
+
+
+class AsyncmongoDBCursor(AbstractDBCursor):
+    def __init__(self, asyncmongo_cursor):
+        self.__cursor = asyncmongo_cursor
+
+    @abc.abstractproperty
+    def fetch_next(self):
+        pass
+
+    @abc.abstractmethod
+    def next_object(self):
+        pass
+
+    @abc.abstractmethod
+    def each(self, callback):
+        pass
+
+    @abc.abstractmethod
+    def to_list(self, length=None, callback=None):
+        pass
+
+    @abc.abstractmethod
+    def tail(self, callback):
+        pass
+
+
+class AbstractDBLayer(object, nllog.DoesLogging):
+    """Thin layer asynchronous model to handle network objects.
+
+    Right now this layer doesn't do much, but provides away to intercept
+    the database calls for any future improvements or updates.
+
+    Unfortuantly uncapped collections in Mongo must have a uniqe '_id'
+    field, so this layer will generate one for each insert based on the
+    network resource id and the revision number.
+    """
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self, client, collection_name, capped=False, id_field="id",
+                 timestamp_field="ts"):
+        """Intializes with a reference to the mongodb collection."""
+        nllog.DoesLogging.__init__(self)
+        self.id_field = id_field
+        self.timestamp_field = timestamp_field
+        self.capped = capped
+        self._collection_name = collection_name
+        self._client = client
+
+    @property
+    def collection(self):
+        """Returns a reference to the default mongodb collection."""
+        return self._client[self._collection_name]
+
+    @abc.abstractmethod
+    def find(self, query, callback=None, **kwargs):
+        """Find one or more documents in the collection."""
+        pass
+
+    @abc.abstractmethod
+    def insert(self, data, callback=None, **kwargs):
+        """Inserts data to the collection."""
+        pass
+
+    @abc.abstractmethod
+    def update(self, query, data, callback=None, **kwargs):
+        """Updates data found by query in the collection."""
+        pass
+
+    @abc.abstractmethod
+    def remove(self, query, callback=None, **kwargs):
+        """Remove objects from the database that matches a query."""
+        pass
+
+
+class IncompleteMongoDBLayer(AbstractDBLayer):
+    """
+    Incomplete implementation that is shared between asyncmongo and motor.
+    """
+
+    def _insert_id(self, data):
+        """
+        Utility to make sure that each resource has a proper ID and timestamp.
+
+        returns an '_id' for each object
+        """
+        # First make sure that there is a timestamp
+        timestamp = data.get(self.timestamp_field, None)
+        if timestamp is None:
+            timestamp = int(time.time() * 1000000)
+            data[self.timestamp_field] = timestamp
+        if '_id' not in data:
+            data['_id'] = ObjectId()
+        if self.id_field not in data:
+            data[self.id_field] = str(data['_id'])
+        return data['_id']
+
+    def _insert_callback(self, result, error, object_ids, is_list, callback):
+        """
+        Utility method to make sure that the ObjectIDs are returend to the user.
+        """
+        self.log.debug("_insert_callback.start")
+        if error is not None:
+            callback(None, error=error)
+        if not is_list:
+            object_ids = object_ids[0]
+        self.log.debug("_insert_callback.end")
+        self.log.info("insert.end")
+        callback(object_ids, error=error)
+
+    def insert(self, data, callback=None, **kwargs):
+        """Inserts data to the collection.
+
+        Asyncmongo driver doesn't return the ObjectIDs of the inserted object.
+        This class caches the inserted IDs in advance and issue a find to
+        reterive them.
+        """
+        self.log.info("insert.start")
+        is_list = isinstance(data, list)
+        # It's easier to treat the data as a list
+        if is_list:
+            ldata = data
+        else:
+            ldata = [data]
+
+        obj_ids = [self._insert_id(obj) for obj in ldata]
+        icallback = functools.partial(self._insert_callback,
+                                      object_ids=obj_ids, is_list=is_list,
+                                      callback=callback)
+        return self.collection.insert(data, callback=icallback, **kwargs)
+
+
+class AsyncmongoDBLayer(IncompleteMongoDBLayer):
     """Thin layer asynchronous model to handle network objects.
 
     Right now this layer doesn't do much, but provides away to intercept
@@ -39,20 +197,56 @@ class DBLayer(object, nllog.DoesLogging):
     network resource id and the revision number.
     """
 
-    def __init__(self, client, collection_name, capped=False, Id="id", \
-        timestamp="ts"):
-        """Intializes with a reference to the mongodb collection."""
-        nllog.DoesLogging.__init__(self)
-        self.Id = Id
-        self.timestamp = timestamp
-        self.capped = capped
-        self._collection_name = collection_name
-        self._client = client
+    def find(self, query, callback=None, **kwargs):
+        """Finds one or more elements in the collection."""
+        self.log.info("find")
+        fields = kwargs.pop("fields", {})
+        fields["_id"] = 0
+        cursor = self.collection.find(query, fields=fields,
+                                      callback=callback, **kwargs)
+        return cursor
 
-    @property
-    def collection(self):
-        """Returns a reference to the default mongodb collection."""
-        return self._client[self._collection_name]
+    def _strip_list_callback(self, result, error, callback):
+        """
+        Asyncmongo returns result in list with one element.
+        This utility method extract that element to make it easier for the
+        client to deal with it.
+        """
+        self.log.debug("_strip_list_callback.start")
+        if error is not None:
+            callback(result, error)
+        callback(result[0], error)
+        self.log.debug("_strip_list_callback.end")
+
+    def update(self, query, data, callback=None, **kwargs):
+        """Updates data found by query in the collection."""
+        self.log.info("update.start")
+        ucallback = functools.partial(self._strip_list_callback,
+                                      callback=callback)
+        ret = self.collection.update(query, data, callback=ucallback, **kwargs)
+        self.log.info("update.end")
+        return ret
+
+    def remove(self, query, callback=None, **kwargs):
+        """Remove objects from the database that matches a query."""
+        self.log.info("remove.start")
+        rcallback = functools.partial(self._strip_list_callback,
+                                      callback=callback)
+        ret = self.collection.remove(query, callback=rcallback, **kwargs)
+        self.log.info("remove.end")
+        return ret
+
+
+class MotorDBLayer(IncompleteMongoDBLayer):
+    """Thin layer asynchronous model to handle network objects.
+
+    Right now this layer doesn't do much, but provides away to intercept
+    the database calls for any future improvements or updates.
+
+    Unfortuantly uncapped collections in Mongo must have a uniqe '_id'
+    field, so this layer will generate one for each insert based on the
+    network resource id and the revision number.
+    """
 
     def find(self, query, callback=None, **kwargs):
         """Finds one or more elements in the collection."""
@@ -60,24 +254,11 @@ class DBLayer(object, nllog.DoesLogging):
         fields = kwargs.pop("fields", {})
         fields["_id"] = 0
         cursor = self.collection.find(query,
-                                    fields=fields, **kwargs)
-        return cursor.to_list(length=None, callback=callback)
-
-    def _insert_id(self, data):
-        if "_id" not in data and not self.capped:
-            res_id = data.get(self.Id, str(ObjectId()))
-            timestamp = data.get(self.timestamp, int(time.time() * 1000000))
-            data["_id"] = "%s:%s" % (res_id, timestamp)
-            
-    def insert(self, data, callback=None, **kwargs):
-        """Inserts data to the collection."""
-        self.log.info("insert")
-        if isinstance(data, list) and not self.capped:
-            for item in data:
-                self._insert_id(item)
-        elif not self.capped:
-            self._insert_id(data)
-        return self.collection.insert(data, callback=callback, **kwargs)
+                                      fields=fields, **kwargs)
+        if callback is None:
+            return cursor
+        else:
+            return cursor.to_list(length=None, callback=callback)
 
     def update(self, query, data, callback=None, **kwargs):
         """Updates data found by query in the collection."""
@@ -89,3 +270,16 @@ class DBLayer(object, nllog.DoesLogging):
         self.log.info("remove")
         return self.collection.remove(query, callback=callback, **kwargs)
 
+
+class DBLayerFactory(object):
+    """Factory class to load the right DBLayer implementation."""
+
+    @staticmethod
+    def new_dblayer(client, *args, **kwargs):
+        """
+        Creates an instance of the right DBLayer implementatino for the client.
+        """
+        if isinstance(client, asyncmongo.client.Client):
+            return AsyncmongoDBLayer(client, *args, **kwargs)
+        else:
+            return MotorDBLayer(client, *args, **kwargs)
--- periscope/test/db_test.py
+++ periscope/test/db_test.py
@@ -2,16 +2,15 @@
 """
 Databases related tests
 """
-
 import functools
-from periscope.db import DBLayer
+from periscope.db import DBLayerFactory
 from periscope.test.base import PeriscopeTestCase
 from mock import Mock
 import copy
 
 
 class DBLayerIntegrationTest(PeriscopeTestCase):
-
+    """This test suit test againest mongodb directly."""
     def __init__(self, *args, **kwargs):
         super(DBLayerIntegrationTest, self).__init__(*args, **kwargs)
         self.collection_name = "test_collection"
@@ -28,30 +27,29 @@ class DBLayerIntegrationTest(PeriscopeTestCase):
         self.sync_db.drop_collection(self.collection_name)
 
     def test_insert(self):
+        """
+        Test inserting a document into the database.
+        """
         def handle_insert(response, error=None):
+            """The callback function"""
             self.assertIsNone(error)
             self.assertEqual(response, '1')
             self.stop()
 
-        model = DBLayer(self.async_db, self.collection_name)
-        self.assertEqual(model.collection.full_collection_name,
-                    self.async_db[self.collection_name].full_collection_name)
-        model.insert({"_id": "1", "id": "1", "two": 2},  callback=handle_insert)
-        self.wait()
-
-    def test_delete(self):
-        def handle_delete(response, error=None):
-            self.assertIsNone(error)
-            self.assertEqual(response['ok'], 1.0)
-            self.stop()
-
-        self.sync_db[self.collection_name].insert({"two": 2})
-        model = DBLayer(self.async_db, self.collection_name)
-        model.remove({"two": 2},  callback=handle_delete)
+        model = DBLayerFactory().new_dblayer(self.async_db,
+                                             self.collection_name)
+        full_collection_name = model.collection.full_collection_name
+        collection = self.async_db[self.collection_name].full_collection_name
+        self.assertEqual(full_collection_name, collection)
+        model.insert({"_id": "1", "id": "1", "two": 2}, callback=handle_insert)
         self.wait()
 
     def test_find(self):
+        """
+        Test finding a document in the database
+        """
         def handle_find(expected, response, error=None):
+            """The callback function"""
             self.assertIsNone(error)
             self.assertEqual(response, expected)
             self.stop()
@@ -63,69 +61,93 @@ class DBLayerIntegrationTest(PeriscopeTestCase):
         expected = [{u"num": 2}, {u"num": 3}]
         find_callback = functools.partial(handle_find, expected)
 
-        model = DBLayer(self.async_db, self.collection_name)
-        model.find({"num": {"$gte": 2}},
-                    callback=find_callback)
+        model = DBLayerFactory().new_dblayer(self.async_db,
+                                             self.collection_name)
+        model.find({"num": {"$gte": 2}}, callback=find_callback)
         self.wait()
 
     def test_update(self):
+        """
+        Test updating existing field in the database.
+        """
         def handle_update(response, error=None):
+            """The callback function"""
             self.assertIsNone(error)
             self.assertEqual(response['ok'], 1.0)
             self.stop()
 
         self.sync_db[self.collection_name].insert({"num": 1})
-        model = DBLayer(self.async_db, self.collection_name)
+        model = DBLayerFactory().new_dblayer(self.async_db,
+                                             self.collection_name)
         model.update({"num": 1}, {"num": 2}, callback=handle_update)
         self.wait()
 
     def test_remove(self):
+        """
+        Test deleting a document from the database
+        """
         def handle_remove(response, error=None):
+            """The callback function"""
             self.assertIsNone(error)
             self.assertEqual(response['ok'], 1.0)
             self.stop()
 
         self.sync_db[self.collection_name].insert({"num": 1})
-        model = DBLayer(self.async_db, self.collection_name)
+        model = DBLayerFactory().new_dblayer(self.async_db,
+                                             self.collection_name)
         model.remove({"num": 1}, callback=handle_remove)
         self.wait()
 
 
 class DBLayerTest(PeriscopeTestCase):
+    """This test suit mocks mongodb driver."""
     def test_insert(self):
+        """
+        Test inserting a document into the database.
+        """
         # Arrange
         response = [{u'connectionId': 1, u'ok': 1.0, u'err': None, u'n': 0}]
         collection_name = "collection_insert"
         collection = Mock(name=collection_name)
         collection.insert.return_value = None
-        collection.insert.side_effect = lambda *args, **kwargs: kwargs['callback'](response, error=None)
+        call = lambda *args, **kwargs: kwargs['callback'](response, error=None)
+        collection.insert.side_effect = call
         client = {collection_name: collection}
         callback = Mock(name="insert_callback")
-        callback.side_effect = lambda response, error : self.stop()
-        ts = 1330921125000000
-        data = {"id": "1", "ts": ts, "two": 2}
+        callback.side_effect = lambda response, error: self.stop()
+        timestamp = 1330921125000000
+        data = {"id": "1", "ts": timestamp, "two": 2}
         expected = copy.copy(data)
-        expected['_id'] = "1:"+str(ts)
+
         # Act
-        model = DBLayer(client, collection_name)
+        model = DBLayerFactory().new_dblayer(client, collection_name)
         model.insert(data, callback=callback)
         self.wait()
+
         # Assert
-        collection.insert.assert_called_once_with(expected,  callback=callback)
-        callback.assert_called_once_with(response,  error=None)
-    
-    def test_delete(self):
+        self.assertEqual(collection.insert.call_count, 1)
+        inserted = collection.insert.call_args[0][0]
+        obj_id = inserted.pop('_id', None)
+        self.assertIsNotNone(obj_id)
+        self.assertEqual(inserted, expected)
+        callback.assert_called_once_with(obj_id, error=None)
+
+    def test_remove(self):
+        """
+        Test deleting a document from the database
+        """
         response = [{u'connectionId': 1, u'ok': 1.0, u'err': None, u'n': 0}]
         query = {"id": "1", "two": 2}
         collection_name = "collection_remove"
         collection = Mock(name=collection_name)
         collection.remove.return_value = None
-        collection.remove.side_effect = lambda *args, **kwargs: kwargs['callback'](response, error=None)
+        call = lambda *args, **kwargs: kwargs['callback'](response, error=None)
+        collection.remove.side_effect = call
         client = {collection_name: collection}
         callback = Mock(name="remove_callback")
-        callback.side_effect = lambda response, error : self.stop()
+        callback.side_effect = lambda response, error: self.stop()
         # Act
-        model = DBLayer(client, collection_name)
+        model = DBLayerFactory().new_dblayer(client, collection_name)
         model.remove(query, callback=callback)
         self.wait()
         # Assert
@@ -133,43 +155,67 @@ class DBLayerTest(PeriscopeTestCase):
         callback.assert_called_once_with(response, error=None)
 
     def test_find(self):
+        """
+        Test finding a document in the database
+        """
         # Arrange
         response = [{"_id": "2", "num": 2}, {"_id": "3", "num": 3}]
         expected = [{"_id": "2", "num": 2}, {"_id": "3", "num": 3}]
-        query = {"num": {"$gte": 2},}
+        query = {"num": {"$gte": 2}, }
+        # This is mock for the mongodb driver
         collection_name = "collection_find"
-        cursor = Mock(name="cursor"+collection_name)
+        cursor = Mock(name="cursor" + collection_name)
         collection = Mock(name=collection_name)
         collection.find.return_value = cursor
-        #collection.find.side_effect = lambda *args, **kwargs: kwargs['callback'](response, error=None)
-        cursor.to_list.side_effect = lambda *args, **kwargs: kwargs['callback'](response, error=None)
+        call = lambda *args, **kwargs: kwargs['callback'](response, error=None)
+        cursor.to_list.side_effect = call
         client = {collection_name: collection}
+        # Mock for the callback by the driver
         callback = Mock(name="find_callback")
-        callback.side_effect = lambda response, error : self.stop()
+        callback.side_effect = lambda response, error: self.stop()
+
         # Act
-        model = DBLayer(client, collection_name)
+        model = DBLayerFactory().new_dblayer(client, collection_name)
         model.find(query, callback=callback)
         self.wait()
+
         # Assert
         self.assertEqual(collection.find.call_args[0], (query,))
         callback.assert_called_once_with(expected, error=None)
-    
+
     def test_update(self):
+        """
+        Test updating existing field in the database.
+        """
         # Arrange
-        response = [{u'updatedExisting': True, u'connectionId': 1, u'ok': 1.0, u'err': None, u'n': 1}]
+        response = [
+            {
+                u'updatedExisting': True,
+                u'connectionId': 1,
+                u'ok': 1.0,
+                u'err': None,
+                u'n': 1
+            }
+        ]
         query = {"id": 1}
         data = {"num": 2}
         collection_name = "collection_update"
+        # This is mock for the mongodb driver
         collection = Mock(name=collection_name)
         collection.update.return_value = None
-        collection.update.side_effect = lambda *args, **kwargs: kwargs['callback'](response, error=None)
+        call = lambda *args, **kwargs: kwargs['callback'](response, error=None)
+        collection.update.side_effect = call
         client = {collection_name: collection}
+        # Mock for the callback by the driver
         callback = Mock(name="update_callback")
-        callback.side_effect = lambda response, error : self.stop()
+        callback.side_effect = lambda response, error: self.stop()
+
         # Act
-        model = DBLayer(client, collection_name)
+        model = DBLayerFactory().new_dblayer(client, collection_name)
         model.update(query, data, callback=callback)
         self.wait()
+
         # Assert
-        collection.update.assert_called_once_with({'id': 1}, data, callback=callback)
+        collection.update.assert_called_once_with({'id': 1}, data,
+                                                  callback=callback)
         callback.assert_called_once_with(response, error=None)
